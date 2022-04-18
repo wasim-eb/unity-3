46050feb8b206432e88193ebcb8b839f98895316
wasim-eb <Wasse.elb@gmail.com>
2022-04-18 23:06:17 +0000

    Deployment commit

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+os.environ["DATABASE_URL"] = "postgres://hcbfkiumydqcdb:9248199932ff8e97d938c8f7bf858e698abc5fcbb400d82d947896e6a0661c4f@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dcedmks9iu0v2f"
+os.environ["SECRET_KEY"] = "kz(&+0p-3(f7*tu4dkojp2_924p($_9@sp&9h-)j6@&rb&zr@4"
+os.environ["CLOUDINARY_URL"] = "cloudinary://487992663689523:sWiQpm3IP2mfv6gGtLpo_HUSW5k@dfpmwgi7a"
--- unity/settings.py
+++ unity/settings.py
@@ -11,21 +11,28 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+   import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9fodn=y_zk0dvn$!ls@#h-&0k4ggg5etz1r42zc5^(0r2g8*w&'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["unity-food-blog.herokuapp.com", "localhost"]
 
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io/']
 
@@ -37,7 +44,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'blog',
     'widget_tweaks',
     'ckeditor',
@@ -74,7 +83,7 @@ ROOT_URLCONF = 'unity.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, ('templates'))],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -93,11 +102,15 @@ WSGI_APPLICATION = 'unity.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
@@ -138,13 +151,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# STATICFILES_DIRS = (
+#         os.path.join(BASE_DIR, 'static'),
+#     )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
