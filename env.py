0eb4cf35fcb89a4dee235fd4db19386041313005
wasim-eb <Wasse.elb@gmail.com>
2022-04-20 20:01:59 +0000

    Fix database error

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
-os.environ["DATABASE_URL"] = "postgres://hcbfkiumydqcdb:9248199932ff8e97d938c8f7bf858e698abc5fcbb400d82d947896e6a0661c4f@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dcedmks9iu0v2f"
+os.environ["DATABASE_URL"] = "postgres://ysytqpcuqzqxrm:d8596bfe072d014a2b8faf140bd57669d48b9d4d80ee127dc6bb3dc0cc384f8e@ec2-54-170-90-26.eu-west-1.compute.amazonaws.com:5432/dcianq7sm2mnss"
 os.environ["SECRET_KEY"] = "kz(&+0p-3(f7*tu4dkojp2_924p($_9@sp&9h-)j6@&rb&zr@4"
 os.environ["CLOUDINARY_URL"] = "cloudinary://487992663689523:sWiQpm3IP2mfv6gGtLpo_HUSW5k@dfpmwgi7a"
